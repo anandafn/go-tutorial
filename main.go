a5348b38739f746d5c4a8bcce8112a1d7cc3eab7
anandafn <anandanamira03@gmail.com>
2024-08-18 21:13:14 +0700

    Third commit - ready for deployment

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 )
 
 type Todo struct {
-	ID primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"` // The omitempty says that if the value is false, then dont add it to the response
+	ID primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"` // The omitempty says that if the value is false, then dont add it to the response
 	Completed bool `json:"completed"`
 	Body string `json:"body"`
 }
@@ -25,10 +25,13 @@ var collection *mongo.Collection
 func main() {
 	fmt.Println("Hello")
 
-	err := godotenv.Load(".env")
+	if os.Getenv("ENV") != "production" {
+		// Load the .env file if not in production
+		err := godotenv.Load(".env")
 
-	if err != nil {
-		log.Fatal("Error loading .env file:", err)
+		if err != nil {
+			log.Fatal("Error loading .env file:", err)
+		}
 	}
 
 	MONGODB_URI := os.Getenv("MONGODB_URI")
@@ -54,6 +57,11 @@ func main() {
 
 	app := fiber.New()
 
+	// app.Use(cors.New(cors.Config{
+	// 	AllowOrigins: "http://localhost:5173",
+	// 	AllowHeaders: "Origin,Content-Type,Accept",
+	// }))
+
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
 	app.Patch("/api/todos/:id", updateTodo)
@@ -62,6 +70,10 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "5000"
+	}
+
+	if os.Getenv("ENV") == "production" {
+		app.Static("/", "./client/dist")
 	}
 
 	log.Fatal(app.Listen("0.0.0.0:" + port))
